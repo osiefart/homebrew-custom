a28c3139356b424d6a5dfa190b7ccd543d740a40
Olaf Siefart <osiefart@Olafs-Macbook-Pro.fritz.box>
2017-02-16 21:02:57 +0100

    initial commit

---
+++ Casks/atom.rb
@@ -0,0 +1,36 @@
+cask 'myatom' do
+  version '1.14.2'
+  sha256 '0ff141e683e8c3d9c0b17def2db7fe05ba05e7dd1b46bae7f3ae3d7d1782452a'
+
+  # github.com/atom/atom was verified as official when first introduced to the cask
+  url "file:///Users/osiefart/Downloads/atom-mac-1.14.2.zip"
+  appcast 'https://github.com/atom/atom/releases.atom',
+          checkpoint: 'f79edc88be01a0fbc9ebd94230475943c8c3d70b336ec5cc69b2b2e2e6bc7629'
+  name 'Github Atom'
+  homepage 'https://atom.io/'
+
+  auto_updates true
+  depends_on macos: '>= :mountain_lion'
+
+  app 'Atom.app'
+  binary "#{appdir}/Atom.app/Contents/Resources/app/apm/node_modules/.bin/apm", target: 'apm'
+  binary "#{appdir}/Atom.app/Contents/Resources/app/atom.sh", target: 'atom'
+
+  postflight do
+    suppress_move_to_applications
+  end
+
+  zap delete: [
+                '~/.atom',
+                '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.github.atom.sfl',
+                '~/Library/Application Support/ShipIt_stderr.log',
+                '~/Library/Application Support/Atom',
+                '~/Library/Application Support/ShipIt_stdout.log',
+                '~/Library/Application Support/com.github.atom.ShipIt',
+                '~/Library/Caches/com.github.atom',
+                '~/Library/Caches/com.github.atom.ShipIt',
+                '~/Library/Preferences/com.github.atom.helper.plist',
+                '~/Library/Preferences/com.github.atom.plist',
+                '~/Library/Saved Application State/com.github.atom.savedState',
+              ]
+end
